5a9dae4502f2a24da4e85c31f127f9d2d6d8afb6
zniq <50171985+zniqco@users.noreply.github.com>
2023-04-09 20:38:32 +0900

    Support env config

--- index.js
+++ index.js
@@ -1,73 +1,76 @@
-const { config } = require('./config.json');
+const fs = require('node:fs');
+const path = require('node:path');
 const { Client, REST, Events, GatewayIntentBits, Collection, Routes } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
-const rest = new REST({ version: '10' }).setToken(config.token);
-const commands = new Collection();
+const commandHandlers = new Collection();
 const messageReceivers = [];
 
-(async () => {
-    // Load modules
-    const fs = require('node:fs');
-    const path = require('node:path');
-    const modules = [];
-    const modulePath = path.join(__dirname, 'modules');
-    const moduleFiles = fs.readdirSync(modulePath).filter(file => file.endsWith('.js'));
+// Configs
+const configFile = fs.existsSync('./config.json') ? require('./config.json') : {};
+const clientId = configFile.CLIENT_ID ?? process.env.BOT_EPSILON_CLIENT_ID;
+const token = configFile.TOKEN ?? process.env.BOT_EPSILON_TOKEN;
 
-    for (const file of moduleFiles) {
-        const filePath = path.join(modulePath, file);
-        const module = require(filePath);
-    
-        if ('commandData' in module && 'commandExecutor' in module) { // Has command?
-            modules.push(module.commandData.toJSON());
-            commands.set(module.commandData.name, module.commandExecutor);
-        }
+// Modules
+const modules = [];
+const modulePath = path.join(__dirname, 'modules');
+const moduleFiles = fs.readdirSync(modulePath).filter(file => file.endsWith('.js'));
 
-        if ('messageReceiver' in module) {
-            messageReceivers.push(module.messageReceiver);
-        }
+for (const file of moduleFiles) {
+    const filePath = path.join(modulePath, file);
+    const module = require(filePath);
+
+    if ('commandData' in module && 'commandExecutor' in module) { // Has command?
+        modules.push(module.commandData.toJSON());
+        commandHandlers.set(module.commandData.name, module.commandExecutor);
     }
 
-    // Login
-    client.once(Events.ClientReady, async c => {
-        console.log(`Logged in as '${c.user.tag}'`);
+    if ('messageReceiver' in module) {
+        messageReceivers.push(module.messageReceiver);
+    }
+}
 
-        // Push commands
-        try {
-            const data = await rest.put(
-                Routes.applicationCommands(config.clientId),
-                { body: modules },
-            );
-    
-            console.log(`${data.length} command(s) loaded`);
-        } catch (e) {
-            console.error(e);
-        }
-    });    
+// Set commands
+const rest = new REST({ version: '10' }).setToken(token);
 
-    // Listen commands
-    client.on(Events.InteractionCreate, async interaction => {
-        if (interaction.isChatInputCommand()) {
-            const command = commands.get(interaction.commandName);
+client.once(Events.ClientReady, async c => {
+    console.log(`Logged in as '${c.user.tag}'`);
 
-            if (command) {
-                try {
-                    await command(interaction);
-                } catch (e) {
-                    console.error(e);
+    // Push commands
+    try {
+        const data = await rest.put(
+            Routes.applicationCommands(clientId),
+            { body: modules },
+        );
 
-                    await interaction.reply({ content: '명령어를 실행하는데 실패했습니다.', ephemeral: true });
-                }
+        console.log(`${data.length} command(s) loaded`);
+    } catch (e) {
+        console.error(e);
+    }
+});    
+
+// Events
+client.on(Events.InteractionCreate, async interaction => {
+    if (interaction.isChatInputCommand()) {
+        const command = commandHandlers.get(interaction.commandName);
+
+        if (command) {
+            try {
+                await command(interaction);
+            } catch (e) {
+                console.error(e);
+
+                await interaction.reply({ content: '명령어를 실행하는데 실패했습니다.', ephemeral: true });
             }
         }
-    });
+    }
+});
 
-    // Listen messages
-    client.on(Events.MessageCreate, message => {
-        if (message && !message.reference && message.author.id != client.user.id) {
-            for (const receiver of messageReceivers)
-                receiver(message);
-        }
-    });    
-})();
+client.on(Events.MessageCreate, message => {
+    if (message && !message.reference && message.author.id != client.user.id) {
+        for (const receiver of messageReceivers)
+            receiver(message);
+    }
+});
 
-client.login(config.token);
+// Login
+client.login(token);
